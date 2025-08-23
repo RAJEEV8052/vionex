11a0d66bcb535a8cf4c918b7b74b04676e4418b7
Rajeev Nayan Singh <rajeevbuta3@gamil.com>
2025-08-23 19:04:53 +0530

    03-authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,54 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import { HoverCard } from "@radix-ui/react-hover-card";
+import { Input } from "@/components/ui/input";
 
+import { useState } from "react";
+import { authClient } from "@/lib/auth.client";
 export default function Home() {
-  return <HoverCard>hello</HoverCard>;
+  const { data: session } = authClient.useSession();
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const onSubmit = () => {
+    authClient.signUp.email(
+      { email, password, name },
+      {
+        onError: () => {
+          window.alert("Error creating user");
+        },
+        onSuccess: () => {
+          window.alert("User created");
+        },
+      }
+    );
+  };
+  if (session) {
+    return (
+      <div className=" flex flex-col p-4 gap-y-4">
+        <p>Logged in as {session.user.name}</p>
+        <button onClick={() => authClient.signOut()}>Signout</button>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <Input
+        placeholder="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Input
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        placeholder="password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button onClick={onSubmit}>Create User</Button>
+    </div>
+  );
 }
